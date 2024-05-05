4c57ce1c54ca2bbeef966cedecd97a85ac602651
Shohei.Mizuno <32non23ca@gmail.com>
2024-05-04 21:08:16 -0700

    TypeScript for JavaScript Programmers

--- app/TypeScriptForJavaScriptProgrammers/1_DefiningTypes.ts
+++ app/TypeScriptForJavaScriptProgrammers/1_DefiningTypes.ts
@@ -35,5 +35,64 @@ class UserAccount {
 
 const user3: User = new UserAccount("Murphy", 1);
 
-const dog = "aaaa";
-dog = "bbbb";
+function deleteUser(user: User) {
+  console.log(`delete ${user}`);
+}
+
+// composing type
+
+// union
+type Mybool = true | false;
+
+type WindowStates = "open" | "closed" | "minimized";
+type LockStates = "locked" | "unlocked";
+type PositiveOddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+
+const ws: WindowStates = "open";
+
+function getLength(obj: string | string[]) {
+	if(typeof obj === "string") {
+		return [obj];
+	}
+}
+
+const strVal = "string desu"
+console.log(getLength(strVal));
+
+const strArr = "string arr desu"
+console.log(getLength(strArr));
+
+// Generics
+
+type StringArray = Array<string>;
+type NumberArray = Array<number>;
+type ObjectWithNameArray = Array<{ name: string }>;
+
+interface Backpack<Type> {
+	add: (obj: Type) => void;
+	get: () => Type;
+}
+
+// Backpac未実装
+declare const backpack: Backpack<string>;
+
+const object = backpack.get();
+
+// backpack.add(23)
+
+// Structural Type System
+
+interface Point {
+	x: number;
+	y: number;
+}
+
+function logPoint(p: Point) {
+	console.log(`${p.x}, ${p.y}`)
+}
+
+const point = { x:12, y:26 }
+logPoint(point)
+
+
+
--- app/TypeScriptForJavaScriptProgrammers/Note.md
+++ app/TypeScriptForJavaScriptProgrammers/Note.md
@@ -14,3 +14,49 @@ https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
 - ES6で導入されてClass
 - そのため、トンスパイル後のコードにも残る
 
+> [!TIP]
+> To learn the type of a variable, use typeof:
+> Type	Predicate
+> string	typeof s === "string"
+> number	typeof n === "number"
+> boolean	typeof b === "boolean"
+> undefined	typeof undefined === "undefined"
+> function	typeof f === "function"
+> array	Array.isArray(a)
+
+## declare (アンビエント宣言)
+
+- cypressのカスタムコマンド用意するときのやつ。タイプ定義ファイルとか
+1. JSで定義された関数などをTSの型チェック対象に入れる
+1. 型定義ファイルで関数を定義する
+
+https://www.typescriptlang.org/docs/handbook/declaration-files/by-example.html
+
+## Generics
+
+## Structural Type System (構造的型付け)
+
+- 実際は、「構造的部分型」= 構造的型付け + 部分型
+    - Javaなどは「名前的部分型」。継承(extends)を通じて型間の親子関係が形成される
+
+以下に型付けのアプローチがまとまっている
+
+https://typescriptbook.jp/reference/values-types-variables/structural-subtyping
+
+## ダックタイピング
+
+- Goなどと同じようにTypeScriptもダックタイピング
+
+> [!TIP]
+> ダックタイピングは、オブジェクトの型よりもオブジェクトの持つメソッドやプロパティが何であるかによってオブジェクトを判断するプログラミングスタイルです
+
+
+
+
+
+
+
+
+
+
+
